e2d82d2e268dfadf0983968ee9462ed5665db4fd
CsixHsix <tree3841@gmail.com>
2021-12-09 13:07:18 +0900

    project upload first time

---
+++ src/main/java/com/greenart/school_mangement/SchoolMangementApplication.java
@@ -0,0 +1,13 @@
+package com.greenart.school_mangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolMangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenart/school_mangement/SchoolMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenart.school_mangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
